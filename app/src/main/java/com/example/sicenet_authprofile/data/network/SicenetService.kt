93bde5c83e244db3b3386b9ba09f4484a7426756
lizetglm <lizetglm@gmail.com>
2026-02-15 14:51:19 -0600

    Implementacion de variables request en Service

--- app/src/main/java/com/example/sicenet_authprofile/data/network/SicenetService.kt
+++ app/src/main/java/com/example/sicenet_authprofile/data/network/SicenetService.kt
@@ -81,7 +81,7 @@ val califFinalRequest = """
     <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
       <soap:Body>
         <getAllCalifFinalByAlumnos xmlns="http://tempuri.org/">
-          <bytModEducativo>unsignedByte</bytModEducativo>
+          <bytModEducativo>%s</bytModEducativo>
         </getAllCalifFinalByAlumnos>
       </soap:Body>
     </soap:Envelope>
